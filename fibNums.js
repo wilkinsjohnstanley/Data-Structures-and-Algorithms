60d18c36d58370f43924efea842c7f0ebefa5b97
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-22 20:22:10 +0900

    Update fibNums.js

--- fibNums.js
+++ fibNums.js
@@ -12,7 +12,7 @@ fibonacci(12) = 0,1,1,2,3,5,8,13,21,34,55,89
 function fibonacci(n){
     const fib = [0,1]
    
-        for(let i = 2; i < n; i++) { //If you did <=, consider not doing that. We can i to be less than n because we're comparing the two nums that preceed it, and never n and the num directly behind it 
+        for(let i = 2; i < n; i++) { //If you did <=, consider not doing that. We're comparing the two nums that preceed 'i', and never n and the num directly behind it 
             fib[i] = fib[i-1] + fib[i-2] //If you commented this out the result would be: Array [ 0, 1 ] no matter what input 
         }
         return fib;
