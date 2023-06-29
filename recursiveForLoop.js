57af7a143cd4cce2fdac74b794cf72a88127d644
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-29 13:46:34 +0900

    Update recursiveForLoop.js

--- recursiveForLoop.js
+++ recursiveForLoop.js
@@ -6,10 +6,21 @@ That parameter is what's checked for the base case.
 ★The parameter is what we looo over (int x).
 ★The base case (x <= 10) is the stop condition of the loop.
 */
+// function recursiveFor(x){ //define it
+//     if(x <= 10){          //base case (the condition to stop the recursive loop)
+        
+//         console.log("The recursions start coming and they don't stop coming");
+//         recursiveFor(x+=1); //call itself and increment each time, so the base case will be reached
+//     }
+    
+// }
+
+// recursiveFor(1) //call the function with at least one parameter
+
 function recursiveFor(x){ //define it
     if(x <= 10){          //base case (the condition to stop the recursive loop)
         
-        console.log("The recursions start coming and they don't stop coming");
+        document.writeln("The recursions start coming and they don't stop coming"+"<br>");
         recursiveFor(x+=1); //call itself and increment each time, so the base case will be reached
     }
     
