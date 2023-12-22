d5bfa1ab76a0e6ff2f1933055ccc50acc682cf89
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-12-22 13:25:54 -0600

    Create arrayTimeComplexity.js

---
+++ arrayTimeComplexity.js
@@ -0,0 +1,7 @@
+//An array is an ordered collection of values
+const odd = [1, 3, 5, 7, 9, 11]
+/*
+If you Insert or Remove from the end of the array the time complexity is O(1).
+If you Insert or Remove from the begnning it's Linear O(n)
+This is because the index must be reset for every remaining element in the array.
+  */
