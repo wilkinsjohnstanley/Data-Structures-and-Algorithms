d01594332747298d74adf752d2b6a8345e66ea9e
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-12-22 13:28:52 -0600

    Update arrayTimeComplexity.js

--- arrayTimeComplexity.js
+++ arrayTimeComplexity.js
@@ -4,4 +4,13 @@ const odd = [1, 3, 5, 7, 9, 11]
 If you Insert or Remove from the end of the array the time complexity is O(1).
 If you Insert or Remove from the begnning it's Linear O(n)
 This is because the index must be reset for every remaining element in the array.
+
+Accessing an element is constant time complexity because 
+fetching the 1st element is no different from fetching an element
+at the position of 100,000
+
+Search - O(n)
+Push/Pop - O(1)
+Shift / unshift / concat / slice / splice - O(n)
+
   */
